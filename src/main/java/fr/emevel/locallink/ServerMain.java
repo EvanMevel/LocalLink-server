e79c14ef3a866be33a599cf0247d7089506bdc8a
EvanMevel <evan.mevel@epitech.eu>
2023-02-19 18:14:33 +0100

    FEAT added simple client and server handshake protocol

---
+++ src/main/java/fr/emevel/locallink/ServerMain.java
@@ -0,0 +1,19 @@
+package fr.emevel.locallink;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ServerMain {
+
+    public static void main(String[] args) throws IOException {
+        LocalLinkServer server = new LocalLinkServer(4242);
+
+        server.start();
+
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextLine();
+
+        server.stop();
+    }
+}
